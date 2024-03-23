8951f245ee10778ce8ac50916e783b66e1cbae0e
RaedYassin <raedyassin02468@gmail.com>
2024-03-23 16:31:08 +0200

    23-3-2024-second-upload

--- src/components/Prayer.jsx
+++ src/components/Prayer.jsx
@@ -5,7 +5,7 @@ import Typography from '@mui/material/Typography';
 export default function Prayre({img,prayer,time}) {// this error by validation on props to fix it use proptypes package but don't work with me now
     let widthScreen = window.innerWidth;
   return (
-    <Card sx={{ maxWidth: 346 }} style={{width: widthScreen <= 768 ?"35vw":"13vw",marginTop:"40px" }}>
+    <Card sx={{ maxWidth: 346 }} style={{width: widthScreen <= 1200 ?(widthScreen>=768?"50vw":"35vw"):"13vw",marginTop:"40px" }}>
       <CardMedia
         sx={{ height: 140 }}
         image={img}
